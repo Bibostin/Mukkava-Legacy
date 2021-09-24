ec97e4498bc757f8407de8020954e50538af1bee
za <zacharygoggin@btconnect.com>
2021-09-24 18:59:06 +0100

    added basic File IO setup, does not yet have access limiting for threads.

--- server/server.py
+++ server/server.py
@@ -1,7 +1,7 @@
-#Library-Setup
+# Library-Setup
 import socket
 import threading
-import configparser
+import toml
 import datetime
 import logging
 
@@ -11,66 +11,97 @@ logging.basicConfig(
     filename=f'logs/log-{datetime.datetime.now()}',
     level='DEBUG',
 )
-config = configparser.ConfigParser(); 
-config.server = config.read('serverconf.ini')  #Contains server configuration.
-#config.blacklist = config.read('blacklist.txt')#File used to store banned IP's.
-#config.greylist = config.read('greylist.txt')  #File used to tempoarily store IP's that spam connections.
 
-#Main-Vars
+# Main-Vars
+config = toml.load('config.toml')  #This returns a dictionary, check config.toml for variables in use.
 serveraddress = socket.gethostbyname(socket.gethostname())
-servername = config.get('basic', 'servername')
-voip_port = int(config.get('basic', 'voip_port'))
-control_port = int(config.get('basic', 'control_port'))
-password = config.get('basic', 'password')
-whitelist = config.get('basic', 'whitelist')
+client_sockets = []
+client_addresses = []
+client_objects = []
 
-Client_sockets = []
-Client_addresses = []
-Client_objects = []
-class Client_class:
-    def __init__(self, nickname, deafened, muted): #self "represents" the individual object instance of a class
-            self.nickname = nickname
-            self.deafened = deafened
-            self.muted = muted
-            self.priority_speaker = False
+class Client_Class:
+    def __init__(self, nickname, deafened, muted):  # self "represents" the individual object instance of a class
+        self.nickname = nickname
+        self.deafened = deafened
+        self.muted = muted
+        self.priority_speaker = False
 
-#Server Initialisation
 
-logging.info(f' Starting a server with name {servername} on {serveraddress}:{voip_port} Server password is {password}')
+# Server Initialisation
+logging.info(f' Starting a server with name {config["servername"]} on {serveraddress} Server password is {config["password"]}')
 
 text_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-text_server.bind((serveraddress, control_port))
+text_server.bind((serveraddress, config['control_port']))
 text_server.listen()
+logging.info(f' TCP Text and control socket started, listening on port {config["control_port"]}')
 
 voip_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-voip_server.bind((serveraddress, voip_port))
-voip_server.listen()  #UDP DOESNT "LISTEN" THIS NEEDS TO BE CHANGED FOR EITHER AN APPROPRIATE METHOD OR USERMADE FUNC CALL.
+voip_server.bind((serveraddress, config["voip_port"]))
+logging.info(f' UDP VOIP socket started, listening on port {config["voip_port"]}')
 
+#FILE IO //TO DO: INCLUDE LIMITS ON ACCESS TO FILES
+def file_operation(list, operation, ip): #Handles the required file IO for the whitelist and blacklist (and hopefully greylist!)
+    if operation == "check": #check if there is an occurance of a given IP in whitelist or blacklist and give feedback of this.
+        input_file = open(f'{list}.txt', 'r')
+        if ip in input_file.readlines():
+            input_file.close()
+            return True
+        else:
+            input_file.close()
+            return False
 
+    elif operation == "append": #append a given IP to the whitelist or blacklist
+        if file_operation(list, "check", ip) is False:
+            input_file = open(f'{list}.txt', 'a')
+            input_file.write('\n')
+            input_file.write(f'{ip}')
+            input_file.close()
+        else: logging.debug('attempted to add an allready present IP.')
 
+    elif operation == "remove": #Remove a given IP from the whitelist or blacklist by checking
+        if file_operation(list, "check", ip) is True:
+            input_file = open(f'{list}.txt', 'r')
+            line_list = input_file.readlines()
+            input_file.close()
+            del line_list[line_list.index(ip)]
+            output_file = open(f'{list}.txt', 'w+')
+            for lines in line_list:
+                output_file.write(lines)
+            output_file.close()
+        else: logging.debug('attempted to remove a non present IP.')
+    else: logging.debug(f'Invalid use of file_operation function, with params: {list} {operation} {ip}')
 
-#VOIP
+# VOIP
 
-#Text
-def text_broadcast():
-    pass
+# TEXT AND CONTROL
+def text_unicast(client_socket, message): #Send a message directly to a specific client (e.g. command feedback)
+    client_socket.send(message)
+
+def text_multicast(client_socket_list, message): #Send a message to a list of sockets not specified by the Client_sockets array.
+    for client_socket in client_socket_list:
+        client_socket.send(message)
+
+def text_broadcast(message): #Broadcast message to all established sockets the server knows (I.E. for Chat messages)
+    for client_socket in client_sockets:
+        client_socket.send(message)
 
 def text_client_handler():
     pass
     while True:
         client_socket, address = text_server.accept()
-        logging.debug("client connected from {}".format(address))
+        logging.info(f"client connected from {address}")
 
-        client_socket.send("REQ-CLIENT-STATUS".encode('ascii'))
-        client_socket_return = socket.recv(1024).decode('ascii')
-        client_object = Client_class(client_socket_return.nickname, client_socket_return.deafened, client_socket_return.muted)
+        client_socket.send("REQ-CLIENT-STATUS".encode('utf-8')) #UTR-8 offers support for far more charectertypes / Unicode
+        client_socket_return = client_socket.recv(1024).decode('utf-8')
+        client_object = Client_Class(client_socket_return.nickname, client_socket_return.deafened, client_socket_return.muted)
 
-        Client_objects.append(client_object)
-        Client_addresses.append(address)
-        Client_sockets.append(socket)
+        client_objects.append(client_object)
+        client_addresses.append(address)
+        client_sockets.append(socket)
 
-def text_server_reciever():
+def text_server_receiver():
     pass
-#Encryption
 
-#Administration
+# Encryption
+
+# Administration
