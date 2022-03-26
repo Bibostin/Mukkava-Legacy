7fbcead0f08e297028860f82d24fa59787ea5569
Za <zacharygoggin@btconnect.com>
2022-03-26 20:08:45 +0000

    Openal written off, moving on to sounddevice

--- lib-testing/soundevice_test.py
+++ lib-testing/soundevice_test.py
@@ -1,9 +1,10 @@
 import sounddevice as sd
+import pyogg
 import numpy
+import math
 
-sd.default.samplerate = 48000
-sd.default.channels = 2, 2
-duration = 5 #seconds
+sd.default.samplerate = 48000 #48Khz sampling rate
+sd.default.channels = 2, 2 #2 channels for input two channels for output
 
 print(f"Sound devices available to you are: \n {sd.query_devices()}\n")
 input_Dev = int(input("Please enter the ID value of the input device you wish to use: "))
@@ -11,10 +12,14 @@ output_Dev = int(input("Please enter the ID value of the output device you wish 
 sd.default.device = input_Dev, output_Dev
 print(f"default devices set to: {sd.default.device}")
 
-input("hit enter to test recording")
-myrecording = sd.rec(int(duration * sd.default.samplerate))
-sd.wait() # optional
+input("hit enter to test recording... ")
+testrecording = sd.rec(int(1 * sd.default.samplerate))
+print("RECORDING")
+sd.wait()  # optional
 
-input("Press enter to test playback")
-sd.play(myrecording)
+input("Press enter to test playback... ")
+sd.play(testrecording)
+print("PLAYING")
 sd.wait()
+
+
--- py-application/audio.py
+++ py-application/audio.py
@@ -1,6 +1,5 @@
 """
 #DESIGN NOTES
-
 This module is responsible for the following major tasks:
     Detecting microphone activation, and buffering user speach for serialization (asynchronously)
     Playback of received co-user audio streams (asynchronously)
@@ -13,29 +12,3 @@ https://python-sounddevice.readthedocs.io/en/0.4.4/usage.html# and https://pypi.
 https://github.com/orion-labs/opuslib - considered, but dropped due to lack of support and updates, likely wont work with later libopus verisons.
 https://github.com/Zuzu-Typ/PyOpenAL - Tested, but decided not to use as it doesn't have support for openAL's recording functions, Streaming audio seems funky too.
 """
-
-# # Win32 platforms
-# set PYAL_DLL_PATH=C:\path\to\fancy_project\third_party
-#
-# # Unix/Posix-alike environments - bourne shells export
-# PYAL_DLL_PATH=/path/to/fancy_project/third_party
-#
-# # Unix/Posix-alike environments - C shells setenv PYAL_DLL_PATH
-# /path/to/fancy_project/third_party
-
-
-import time
-from openal import *
-from pyogg import *
-
-
-def PlayAudiosStream(filepath):
-
-    source = oalStream("testing/sweep.opus")
-    source.play()
-
-    while source.get_state() == AL_PLAYING:
-        time.sleep(1)
-    oalQuit()
-
-openal.audio.Sound
