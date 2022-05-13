6d82c45488821acca18dae6fd9756cd147609df0
za <zacharygoggin@btconnect.com>
2022-05-13 23:19:38 +0100

    removing print statements for testing

--- mukkava/mukkava_socket.py
+++ mukkava/mukkava_socket.py
@@ -181,9 +181,7 @@ class NetStack:  # IPv4 TCP Socket stack for receiving text and command packets
                         try: data, message_type = outbound_socket.recieve_data()
                         except nacl.exceptions.CryptoError: continue
                         if message_type == "TEXT": print(f"<:OUTp:{outbound_socket.peer_address}:{data}")
-                        elif message_type == "VOIP":
-                            outbound_socket.audio_out_buffer_instance.put(data)
-                            print("recieved voice data")
+                        elif message_type == "VOIP": outbound_socket.audio_out_buffer_instance.put(data)
                         else: print(f"<:OUT recieved a message from {outbound_socket.peer_address} with bad message type tagging")
                 audio_out.process_input()
 
