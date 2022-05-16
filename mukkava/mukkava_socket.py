b2586c1eeca12e630671b3bbff6dca53e1cf6c25
Za <zacharygoggin@btconnect.com>
2022-05-16 16:58:18 +0100

    changes to remove cross processor socket removal

--- mukkava/mukkava_socket.py
+++ mukkava/mukkava_socket.py
@@ -192,14 +192,10 @@ class NetStack:  # IPv4 TCP Socket stack for receiving text and command packets
                         try:
                             if voice_data: inbound_socket.send_data(voice_data, "VOIP")  # if we have voice data, send it
                             if text_data: inbound_socket.send_data(text_data, "TEXT")  # if we have text data, send it
-                        except (ConnectionAbortedError, ConnectionResetError) as error:
+                        except ConnectionError:
                             print(f"<:INp: Connection to {inbound_socket.peer_address} dropped, closing local end of connection")
-                            outbound_socket = self.check_for_existing_socket(inbound_socket.peer_address, "outbound_sockets")
-                            outbound_socket.socket.close
                             inbound_socket.socket.close()
-                            del outbound_socket.audio_out_buffer_instance
                             self.sockets_info["inbound_sockets"].remove(inbound_socket)
-                            self.sockets_info["outbound_sockets"].remove(outbound_socket)
 
             else: audio_in.instream.stop()  # there are no inbound sockets, so stop the audio input stream
 
@@ -217,14 +213,11 @@ class NetStack:  # IPv4 TCP Socket stack for receiving text and command packets
                             if message_type == "TEXT": print(f"<:OUTp:{outbound_socket.peer_address}:{data}")
                             elif message_type == "VOIP": outbound_socket.audio_out_buffer_instance.put(data)
                         except nacl.exceptions.CryptoError: continue  # If we encounter a buffer overflow due to a difference in processing speed, simply ignore it.
-                        except (ConnectionAbortedError, ConnectionResetError) as error:
+                        except ConnectionError:
                             print(f"<:INp: Connection to {outbound_socket.peer_address} dropped, closing local end of connection")
-                            inbound_socket = self.check_for_existing_socket(outbound_socket.peer_address, "inbound_sockets")
                             outbound_socket.socket.close()
-                            inbound_socket.socket.close()
                             del outbound_socket.audio_out_buffer_instance
                             self.sockets_info["outbound_sockets"].remove(outbound_socket)
-                            self.sockets_info["inbound_sockets"].remove(inbound_socket)
 
                 audio_out.process_input()  # mix all the audio data we recieved from the readable sockets into one numpy array
             else:audio_out.outstream.stop()  # there are no outbound sockets, so stop the audio output stream
